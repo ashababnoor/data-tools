40df8b02c354c85fc8287c7b1474acd35685086b
Shabab Noor <ashababnoor@gmail.com>
2023-11-13 23:30:06 +0600

    added code to prettify markdown table in json-to-markdown-table

---
+++ data-tools/json-to-markdown-table/prettify_table.py
@@ -0,0 +1,30 @@
+from tabulate import tabulate
+
+def prettify_markdown_table(markdown_table):
+    # Split the input string into rows
+    rows = markdown_table.strip().split('\n')
+
+    # Split each row into columns
+    table_data = [row.split('|') for row in rows]
+
+    # Remove leading and trailing whitespaces from each column
+    table_data = [[col.strip() for col in row] for row in table_data]
+
+    # Remove the first and last elements (empty strings) from each row
+    table_data = [row[1:-1] for row in table_data]
+
+    # Use tabulate to create a pretty table
+    pretty_table = tabulate(table_data, headers="firstrow", tablefmt="pipe")
+
+    return pretty_table
+
+# Example usage
+markdown_input = """
+| Name  | Age | City    |
+| Alice | 25  | New York|
+| Bob   | 30  | London  |
+| Carol | 22  | Paris   |
+"""
+
+prettified_table = prettify_markdown_table(markdown_input)
+print(prettified_table)
