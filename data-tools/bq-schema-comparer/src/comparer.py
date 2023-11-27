8ccab2ac1bb0ac9b8038be8e86bd498381e60790
Shabab Noor <ashababnoor@gmail.com>
2023-11-27 21:44:54 +0600

    added uncommon columns list in schema compare function in comparer tool

--- data-tools/bq-schema-comparer/src/comparer.py
+++ data-tools/bq-schema-comparer/src/comparer.py
@@ -8,6 +8,7 @@ def load_schema(file_path):
 def compare_schemas(schema1, schema2):
     common_columns = []
     similar_columns = []
+    uncommon_columns = []
     
     for col1 in schema1:
         for col2 in schema2:
@@ -15,7 +16,9 @@ def compare_schemas(schema1, schema2):
                 common_columns.append((col1['name'], col1['type']))
             elif col1['type'] == col2['type'] and fuzz.ratio(col1['name'], col2['name']) > 80:
                 similar_columns.append((col1['name'], col2['name'], col1['type']))
-            
-
-    return common_columns, similar_columns
-
+    
+    # Find uncommon columns
+    all_columns = set((col['name'], col['type']) for col in schema1) | set((col['name'], col['type']) for col in schema2)
+    uncommon_columns = all_columns - set(common_columns) - set(similar_columns)
+    
+    return common_columns, similar_columns, list(uncommon_columns)
